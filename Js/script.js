17e66123f1d9c279ce7f41cc4fa0444997eda181
headlessNode <harriskhan047@outlook.com>
2023-09-13 15:01:49 +0500

    Add animation to the dialog box

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,9 @@
 let addBook = document.querySelector('.add-btn');
-let dialogForm = document.querySelector('.form');
+let dialogForm = document.querySelector('.dialog');
 let bookTitleSelector = document.querySelector('.book-title');
 let authorSelector = document.querySelector('.author');
 let nPagesSelector = document.querySelector('.nPages');
+let submitBtn = document.querySelector('.submit-btn');
 
 const myLibrary = [];
 
@@ -26,7 +27,15 @@ function addBookToLibrary(book){
 
 addBook.addEventListener('click', ()=>{
     dialogForm.showModal();
+    dialogForm.style.top = '35%';
+    dialogForm.style.opacity = '1';
 });
 
 
 // on clicking the sumbit btn on the addBookForm you should create a function that takes in user input, create book object based on that input, and store that object in the array.
+submitBtn.addEventListener('click', ()=>{
+    event.preventDefault();
+    dialogForm.close();
+    dialogForm.style.top = '-30%';
+    dialogForm.style.opacity = '0';
+})
