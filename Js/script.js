cb4ff2aada250661382c085d6963081817d157f4
headlessNode <harriskhan047@outlook.com>
2023-09-13 17:16:15 +0500

    Add validation checks to the form inside dialogBox

--- Js/script.js
+++ Js/script.js
@@ -4,6 +4,7 @@ let bookTitleSelector = document.querySelector('.book-title');
 let authorSelector = document.querySelector('.author');
 let nPagesSelector = document.querySelector('.nPages');
 let submitBtn = document.querySelector('.submit-btn');
+let validationFailed = document.querySelector('.fail-msg');
 
 const myLibrary = [];
 
@@ -34,8 +35,56 @@ addBook.addEventListener('click', ()=>{
 
 // on clicking the sumbit btn on the addBookForm you should create a function that takes in user input, create book object based on that input, and store that object in the array.
 submitBtn.addEventListener('click', ()=>{
-    event.preventDefault();
-    dialogForm.close();
-    dialogForm.style.top = '-30%';
-    dialogForm.style.opacity = '0';
+
+    //The input fields must not be empty. Except the checkbox
+    if(bookTitleSelector.value === '' || authorSelector.value === '' || nPagesSelector.value === ''){
+        if(bookTitleSelector.value === ''){
+            bookTitleSelector.style.background = '#BF8891';
+        }
+
+        else if(authorSelector.value === ''){
+            authorSelector.style.background = '#BF8891';
+        }
+
+        else if(nPagesSelector.value === ''){
+            nPagesSelector.style.background = '#BF8891';
+        }
+
+        else{
+            bookTitleSelector.style.background = '#BF8891';
+            authorSelector.style.background = '#BF8891';
+            nPagesSelector.style.background = '#BF8891';
+        }
+        
+        validationFailed.textContent = '* Input fields must not be empty.'
+        event.preventDefault();
+    }
+
+    else{
+        //if program is here, it means fields are not empty. Restore default background.
+        validationFailed.textContent = '';
+        bookTitleSelector.style.background = '#C2D0D6';
+        authorSelector.style.background = '#C2D0D6';
+        nPagesSelector.style.background = '#C2D0D6';
+
+        
+        if(bookTitleSelector.value === authorSelector.value){
+            bookTitleSelector.style.background = '#BF8891';
+            authorSelector.style.background = '#BF8891';
+            validationFailed.textContent = '* Book and Author name must not match.'
+            event.preventDefault();
+        }
+
+        else{
+            bookTitleSelector.style.background = '#C2D0D6';
+            authorSelector.style.background = '#C2D0D6';
+
+
+            event.preventDefault();
+        }
+
+    }
+
+    // dialogForm.style.top = '-30%';
+    // dialogForm.style.opacity = '0';
 })
